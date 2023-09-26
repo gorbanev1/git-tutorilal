0db379d9459712017923be2c39be5fc5681c041c
Gorban <gorbanev_i@mail.ru>
2023-09-26 11:10:28 +0300

    урок про this

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-function Post(author, likes) {
+/*function Post(author, likes) {
     const result={};
     this.author=author;
     this.likes=likes;
     return result;    
 }
 const post= new Post('Росбанк',100);
+*/
+function hello(){
+    console.log('Hello', this)
+}
+const person = {
+    name: 'Igor',
+    age: 36,
+    sayHello: hello,
+    sayHelloWindow: hello .bind(document),
+    logInfo: function(job, phone) {
+        console.group(`${this.name} info:`);
+        console.log(`Name is ${this.name}`);
+        console.log(`Age is ${this.age}`);
+        console.log(`job is ${job}`);
+        console.log(`Phone is ${phone}`);
+        console.groupEnd()
+    }
+}
+const lena = {
+    name: 'Elena',
+    age: 23
+}
+const fnLenaInfoLog = person.logInfo.bind(lena,'sekretar', '8342342323423');
+fnLenaInfoLog();
+// person.logInfo.bind(lena, 'sekretar', '8342342323423')();
+// person.logInfo.call(lena, 'sekretar', '8342342323423');
+person.logInfo.apply(lena, ['sekretar', '8342342323423']);
